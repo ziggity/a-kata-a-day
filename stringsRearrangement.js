dfb7685f42930e4a4ecd5b9d4f466ebb402acb3e
Zach <17103355+ziggity@users.noreply.github.com>
2018-01-16 23:22:00 -0800

    Update stringsRearrangement.js

--- stringsRearrangement.js
+++ stringsRearrangement.js
@@ -1,3 +1,45 @@
+const isNext = (string1, string2) =>
+  1 === string1
+          .split('')
+          .reduce((acc, item, index) => acc + +(item !== string2[index]), 0)
+
+
+const arranged = array => {
+  for (let i = 0; i + 1 < array.length; i++) {
+    if (!isNext(array[i], array[i + 1]))
+      return false
+  }
+  return true
+}
+
+const findNext = (string, array) => {
+  let index = -1
+  for (let i = 0; i < array.length && index == -1; i++) {
+    if (isNext(string, array[i]))
+      index = i
+  }
+  return index
+}
+
+const stringsRearrangement = arr => {
+  let next = 0
+
+  arr.sort()
+
+  for (let i = 0; i < arr.length && next >= 0 && !arranged(arr); i++) {
+    next = findNext(arr[i], arr)
+
+    if (next >= 0 && (next > i + 1 || next < i - 1)) {
+      arr.splice(i + 1, 0, arr[next])
+
+      const start = (next < i + 1) ? next : next + 1
+      arr.splice(start, 1)
+    }
+  }
+  return arranged(arr)
+}
+//-------------------------------- 
+// This solution below fails to meet a few hidden tests:
 function stringsRearrangement(inputArray) {
   var bruteForce = function(depth, inputArray) {
     var swap = function(i, j) {
