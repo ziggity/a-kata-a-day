6f34b99316e6584b836e92b06324359748ef10b0
Zach <17103355+ziggity@users.noreply.github.com>
2018-01-28 21:05:16 -0800

    make a map, check if val exists, if doesnt increment
    2nd for loop search for first val 1, if non exist return '_'.

---
+++ firstNotRepeatingChar.js
@@ -0,0 +1,35 @@
+
+function firstNotRepeatingCharacter(s) {
+ const counter ={};
+  for(var i=0; i<s.length; i++){
+    if(counter[s[i]]){
+      counter[s[i]]++
+    } else {
+      counter[s[i]]=1
+    }
+  }
+    for(var j=0; j<s.length; j++){
+      if(counter[s[j]] ==1){
+        return s[j];
+      }
+    }
+    return '_';
+  }
+  
+  
+// Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be asked to do during a real interview.
+
+// Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+
+// Example
+
+// For s = "abacabad", the output should be
+// firstNotRepeatingCharacter(s) = 'c'.
+
+// There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+// For s = "abacabaabacaba", the output should be
+// firstNotRepeatingCharacter(s) = '_'.
+
+// There are no characters in this string that do not repeat.
+
