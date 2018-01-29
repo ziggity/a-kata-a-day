fb6a320f1291ccc4c26c6b83a3bd4b8d394662ec
Zach <17103355+ziggity@users.noreply.github.com>
2018-01-28 20:24:25 -0800

    build a map - loop through it if seen a[i] return it

---
+++ firstDuplicate.js
@@ -0,0 +1,25 @@
+function firstDuplicate(a) {
+
+let seen = {};
+  for(let x of a){
+    if(seen[x]) return x;
+    seen[x] = x;
+  }
+return -1
+}
+  
+  
+
+// Note: Write a solution with O(n) time complexity and O(1) additional space complexity, since this is what you would be asked to do during a real interview.
+
+// Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+
+// Example
+
+// For a = [2, 3, 3, 1, 5, 2], the output should be
+// firstDuplicate(a) = 3.
+
+// There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than than second occurrence of 2 does, so the answer is 3.
+
+// For a = [2, 4, 3, 5, 1], the output should be
+// firstDuplicate(a) = -1.
