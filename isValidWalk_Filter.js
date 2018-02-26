cfa0752973d0592770d1fbf7347903c609882669
Zach <17103355+ziggity@users.noreply.github.com>
2018-02-25 18:41:56 -0800

    Create isValidWalk_Filter.js

---
+++ isValidWalk_Filter.js
@@ -0,0 +1,61 @@
+/*
+You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+*/
+
+function isValidWalk(walk) {
+  var dx = 0
+  var dy = 0
+  var dt = walk.length
+  
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': dy--; break
+      case 's': dy++; break
+      case 'w': dx--; break
+      case 'e': dx++; break
+    }
+  }
+  
+  return dt === 10 && dx === 0 && dy === 0
+}
+
+// other question:
+/*
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+*/
+
+function filter_list(l) {
+  let nums = Array(1000).fill().map((e,i)=>i+1);
+  nums.push(0);
+  let result = [];
+  for(let i=0; i<l.length; i++){
+    if(nums.includes(l[i])){
+      result.push(l[i])
+    }
+  }
+  return result;
+}
+
+// 
+/*
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+*/
+
+function rowSumOddNumbers(n) {
+    return n * n * n;
+}
+
+
