e9ac296f585b1a55f7bdb15e3e5fed7a98ab4147
Zach <17103355+ziggity@users.noreply.github.com>
2018-03-12 23:18:26 -0700

    Update roman_numeral_converter.js

--- roman_numeral_converter.js
+++ roman_numeral_converter.js
@@ -1,3 +1,4 @@
+/*
 Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
 
 Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
@@ -24,6 +25,8 @@ Thoughts:
 2. Build a key / dictionary to pull Characters from
 
 Solution:
+
+*/
 
 
 function solution (number) {
@@ -53,7 +56,7 @@ function deromanize (str) {
 	return number;
 }
 
-Let's take a look at console logs: 
+//Let's take a look at console logs: 
 
 
 
