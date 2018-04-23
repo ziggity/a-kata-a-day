f5d2b6b898b5cfad4a33bbe8a45872ea6d030321
Zach <17103355+ziggity@users.noreply.github.com>
2018-04-23 15:24:52 -0700

    Create isCaseSensPalindrome.js

---
+++ isCaseSensPalindrome.js
@@ -0,0 +1,33 @@
+/*
+Given a string, check if it can become a palindrome through a case change of some (possibly, none) letters.
+
+Example
+
+For inputString = "AaBaa", the output should be
+isCaseInsensitivePalindrome(inputString) = true.
+
+"aabaa" is a palindrome as well as "AABAA", "aaBaa", etc.
+
+For inputString = "abac", the output should be
+isCaseInsensitivePalindrome(inputString) = false.
+
+All the strings which can be obtained via changing case of some group of letters, i.e. "abac", "Abac", "aBAc" and 13 more, are not palindromes.
+
+*/
+
+function isCaseInsensitivePalindrome(inputString) {
+    let lowerC = inputString.toLowerCase().split('').reverse().join('');
+    let lowerA = inputString.toLowerCase();
+    if(lowerC == lowerA) return true
+    else{
+        return false
+    }
+    
+}
+
+// es6
+
+function isCaseInsensitivePalindrome(inputString) {
+    r = inputString.split('').reverse().join('');
+    return inputString.toLowerCase() == r.toLowerCase();
+}
